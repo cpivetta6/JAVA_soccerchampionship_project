22dd6b0175e07a3f467ee67d87f443fbfe9a47a0
Caio Pivetta <caiopivetta6@gmail.com>
2023-01-17 10:45:13 +0100

    Team and Player created

---
+++ src/main/java/com/caiopivetta6/domain/Player.java
@@ -0,0 +1,91 @@
+package com.caiopivetta6.domain;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "player_tb")
+public class Player implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private Instant date;
+	private Double height;
+	
+	public Player () {
+		
+	}
+
+	public Player(Integer id, String name, Instant date, Double height) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.date = date;
+		this.height = height;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public Double getHeight() {
+		return height;
+	}
+
+	public void setHeight(Double height) {
+		this.height = height;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+}
