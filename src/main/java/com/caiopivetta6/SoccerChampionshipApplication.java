1f7c0eedacb4c195c333e2dfc3648f238d9b1b94
Caio Pivetta <caiopivetta6@gmail.com>
2023-01-17 10:22:56 +0100

    project created

---
+++ src/main/java/com/caiopivetta6/SoccerChampionshipApplication.java
@@ -0,0 +1,13 @@
+package com.caiopivetta6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerChampionshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerChampionshipApplication.class, args);
+	}
+
+}
